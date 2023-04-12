8189382570fe56714abc97fe2974b5f55e3a374b
Soumyadip Nandi <30930746+soumyadip1995@users.noreply.github.com>
2023-04-12 22:13:22 +0530

    created spatial_attention.py

---
+++ Attention/spatial_attention.py
@@ -0,0 +1,69 @@
+## from the ldm repo
+
+import torch
+from torch import nn
+import math
+import numpy as np 
+import torch.nn.functional  as F
+from math import sqrt
+from einops import rearrange, repeat
+
+
+class SpatialAttentionHead(nn.Module):
+  def __init__(self, in_channels):
+    super(SpatialAttentionHead, self).__init__()
+    self.in_channels = in_channels
+    self.norm = torch.nn.GroupNorm(num_groups=32, num_channels=in_channels, eps=1e-6, affine=True)
+    self.q = torch.nn.Conv2d(in_channels,
+                                 in_channels,
+                                 kernel_size=1,
+                                 stride=1,
+                                 padding=0)
+    self.k = torch.nn.Conv2d(in_channels,
+                                 in_channels,
+                                 kernel_size=1,
+                                 stride=1,
+                                 padding=0)
+    self.v = torch.nn.Conv2d(in_channels,
+                                 in_channels,
+                                 kernel_size=1,
+                                 stride=1,
+                                 padding=0)
+    self.proj_out = torch.nn.Conv2d(in_channels,
+                                        in_channels,
+                                        kernel_size=1,
+                                        stride=1,
+                                        padding=0)
+    
+
+  def forward(self, x):
+        h_ = x
+        h_ = self.norm(h_)
+        q = self.q(h_)
+        k = self.k(h_)
+        v = self.v(h_)
+
+        # compute attention
+        b,c,h,w = q.shape
+        q = rearrange(q, 'b c h w -> b (h w) c')
+        k = rearrange(k, 'b c h w -> b c (h w)')
+        w_ = torch.einsum('bij,bjk->bik', q, k)
+
+        w_ = w_ * (int(c)**(-0.5))
+        w_ = torch.nn.functional.softmax(w_, dim=2)
+
+        # attend to values
+        v = rearrange(v, 'b c h w -> b c (h w)')
+        w_ = rearrange(w_, 'b i j -> b j i')
+        h_ = torch.einsum('bij,bjk->bik', v, w_)
+        h_ = rearrange(h_, 'b c (h w) -> b c h w', h=h)
+        h_ = self.proj_out(h_)
+
+        return x+h_
+
+
+x = torch.randn(3, 32, 64, 64)
+
+stn = SpatialAttentionHead(32)
+st = stn(x)
+print(st[:5])
