c19085321dedf1b47ad67c906686a46b8b893e50
Soumyadip Nandi <30930746+soumyadip1995@users.noreply.github.com>
2023-05-16 12:19:47 +0530

    LLama trainer and babygpt trainer added to train.

---
+++ train/llama_trainer.py
@@ -0,0 +1,350 @@
+## from lit-llama repo (partially).
+
+
+import math
+from dataclasses import dataclass
+from typing import Optional
+
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+from typing_extensions import Self
+
+@dataclass
+class LLaMAConfig:
+    ## LLaMa parametres
+    block_size: int = 2048
+    vocab_size: int = 32000
+    n_layer: int = 32
+    n_head: int = 32
+    n_embd: int = 4096
+
+
+    @classmethod
+    def from_name(cls, name: str) -> Self:
+        return cls(**llama_configs[name])
+
+
+llama_configs = {
+    "7B": dict(n_layer=32, n_head = 32, n_embd=4096),
+    "13B": dict(n_layer=40, n_head =40, n_embd=5120),
+    "30B": dict(n_layer=60, n_head=52, n_embd=6656),
+    "65B": dict(n_layer=80, n_head =64, n_embd=8192),
+}
+
+
+### other hyperparametres
+
+batch_size = 64 
+max_iters = 1000
+learning_rate = 3e-4
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+eval_interval = 500
+eval_iters = 200
+dropout = 0.2
+
+words = open(r"C:\Users\Soumyadip Nandi\Downloads\policy\language\data\ALL_eminem.txt", 'r', encoding='utf-8').read()
+
+
+chars = sorted(list(set(words)))
+vocab_size = len(chars)
+
+
+string2integer = {ch: i for i, ch in enumerate(chars)}
+integer2string = {i:ch for ch,i in string2integer.items()}
+encode = lambda s: [string2integer[c] for c in s]
+decode = lambda l: ''.join([integer2string[i] for i in l])
+data = torch.tensor(encode(words), dtype = torch.long)
+
+
+## train and split the data
+n = int(0.8*len(data))
+train_data = data[:n]
+val_data = data[n:]
+
+# data loading
+def get_batch(split):
+    # generate a small batch of data of inputs x and targets y
+    data = train_data if split == 'train' else val_data
+    ix = torch.randint(len(data) - config.block_size, (batch_size,))
+    x = torch.stack([data[i:i+ config.block_size] for i in ix])
+    y = torch.stack([data[i+1:i+ config.block_size+1] for i in ix])
+    x, y = x.to(device), y.to(device)
+    return x, y
+
+
+### from pytorch GPT tutorial
+@torch.no_grad()
+def estimate_loss():
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = get_batch(split)
+            logits, loss = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
+
+
+class Attention(nn.Module):
+  def __init__(self, config : LLaMAConfig):
+    super(Attention, self).__init__()
+
+    self.config = config
+    self.atten = nn.Linear(config.n_embd, 3 * config.n_embd)
+    self.projection = nn.Linear(config.n_embd, config.n_embd)
+    self.n_head = config.n_head
+    self.n_embd = config.n_embd
+    self.block_size = config.block_size
+    self.rope_cache: Optional[torch.Tensor] = None
+    self.register_buffer('tril', torch.tril(torch.ones(config.block_size, config.block_size)))
+
+  def forward(self, x):
+    B,T,C = x.size()
+    q, k ,v  = self.atten(x).split(self.n_embd, dim=2)
+    q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+    k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+    v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+
+
+    if self.rope_cache is None:
+      # cache for future forward calls
+      self.rope_cache = build_rope_cache(
+      seq_len=self.block_size,
+      n_elem=self.n_embd // self.n_head, 
+      dtype=x.dtype,
+      device=x.device,
+            )
+
+      q = apply_rope(q, self.rope_cache)
+      k = apply_rope(k, self.rope_cache)
+
+
+
+    #manual implementation of attention
+    #from karpathy
+    # att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+    # att = att.masked_fill(self.tril[:T, :T] == 0, float('-inf'))
+    # att = F.softmax(att, dim=-1)
+    # y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+    # y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
+
+    # efficient attention using Flash Attention CUDA kernels
+    y = F.scaled_dot_product_attention(q, k, v, attn_mask=None, dropout_p=0.0, is_causal=True)
+
+    y = y.transpose(1, 2).contiguous().view(B, T, C)  # re-assemble all head outputs side by side
+
+    # output projection
+    y = self.projection(y)
+
+    return y
+
+class FeedForward(nn.Module):
+  def __init__(self, config: LLaMAConfig) -> None:
+    super().__init__()
+    hidden_dim = 4 * config.n_embd
+    n_hidden = int(2 * hidden_dim / 3)
+
+
+    self.c_fc1 = nn.Linear(config.n_embd, n_hidden, bias=False)
+    self.c_fc2 = nn.Linear(config.n_embd, n_hidden, bias=False)
+    self.c_proj = nn.Linear(n_hidden,  config.n_embd, bias=False)
+    
+  def forward(self, x: torch.Tensor) -> torch.Tensor:
+    x = F.silu(self.c_fc1(x)) * self.c_fc2(x)
+    x = self.c_proj(x)
+    return x
+
+
+### A simple Transformer Block    
+class Transformer(nn.Module):
+  def __init__(self, config : LLaMAConfig) -> None:
+    super(Transformer, self).__init__()
+    self.attention = Attention(config)
+    self.feed_forward = FeedForward(config)
+    self.layer_norm_1 = RMSNorm(config.n_embd)
+    self.layer_norm_2 = RMSNorm(config.n_embd)
+
+  def forward(self, x):
+    
+    x = x + self.attention(self.layer_norm_1(x))
+    x = x + self.feed_forward(self.layer_norm_2(x))
+    return x
+
+class RMSNorm(nn.Module):
+    """Root Mean Square Layer Normalization.
+
+    Derived from https://github.com/bzhangGo/rmsnorm/blob/master/rmsnorm_torch.py. BSD 3-Clause License:
+    https://github.com/bzhangGo/rmsnorm/blob/master/LICENSE.
+    """
+
+    def __init__(self, size: int, dim: int = -1, eps: float = 1e-5) -> None:
+        super().__init__()
+        self.scale = nn.Parameter(torch.ones(size))
+        self.eps = eps
+        self.dim = dim
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # NOTE: the original RMSNorm paper implementation is not equivalent
+        # norm_x = x.norm(2, dim=self.dim, keepdim=True)
+        # rms_x = norm_x * d_x ** (-1. / 2)
+        # x_normed = x / (rms_x + self.eps)
+        norm_x = torch.mean(x * x, dim=self.dim, keepdim=True)
+        x_normed = x * torch.rsqrt(norm_x + self.eps)
+        return self.scale * x_normed
+
+
+def build_rope_cache(seq_len: int, n_elem: int, dtype: torch.dtype, device: torch.device, base: int = 10000) -> torch.Tensor:
+
+    """Enhanced Transformer with Rotary Position Embedding.
+
+    Derived from: https://github.com/labmlai/annotated_deep_learning_paper_implementations/blob/master/labml_nn/
+    transformers/rope/__init__.py. MIT License:
+    https://github.com/labmlai/annotated_deep_learning_paper_implementations/blob/master/license.
+    """
+    # $\Theta = {\theta_i = 10000^{\frac{2(i-1)}{d}}, i \in [1, 2, ..., \frac{d}{2}]}$
+    theta = 1.0 / (base ** (torch.arange(0, n_elem, 2, dtype=dtype, device=device) / n_elem))
+
+    # Create position indexes `[0, 1, ..., seq_len - 1]`
+    seq_idx = torch.arange(seq_len, dtype=dtype, device=device)
+
+    # Calculate the product of position index and $\theta_i$
+    idx_theta = torch.outer(seq_idx, theta).float()
+
+    cache = torch.stack([torch.cos(idx_theta), torch.sin(idx_theta)], dim=-1)
+
+    # this is to mimic the behaviour of complex32, else we will get different results
+    if dtype in (torch.float16, torch.bfloat16, torch.int8):
+        cache = cache.half()
+    return cache
+
+
+def apply_rope(x: torch.Tensor, rope_cache: torch.Tensor) -> torch.Tensor:
+    x = x.transpose(1, 2)
+
+    # truncate to support variable sizes
+    T = x.size(1)
+    rope_cache = rope_cache[:T]
+
+    # cast because the reference does
+    xshaped = x.float().reshape(*x.shape[:-1], -1, 2)
+    rope_cache = rope_cache.view(1, xshaped.size(1), 1, xshaped.size(3), 2)
+    x_out2 = torch.stack(
+        [xshaped[..., 0] * rope_cache[..., 0] - xshaped[..., 1] * rope_cache[..., 1],
+         xshaped[..., 1] * rope_cache[..., 0] + xshaped[..., 0] * rope_cache[..., 1],
+        ], -1)
+
+    x_out2 = x_out2.flatten(3)
+    return x_out2.transpose(1, 2).type_as(x)
+
+class BabyGPTmodel(nn.Module):
+
+    def __init__(self, config):
+        super(BabyGPTmodel, self).__init__()
+
+        assert config.vocab_size is not None
+        assert config.block_size is not None
+
+        self.config = config
+        self.token = nn.Embedding(config.vocab_size, config.n_embd)
+        self.positional_embeddings = nn.Embedding(config.block_size, config.n_embd)
+        self.blocks = nn.Sequential(*[Transformer(config) for _ in range(config.n_layer)])
+        self.ln_f = RMSNorm(config.n_embd, eps = 1e-12) # final layer norm
+        self.lnum_heads = nn.Linear(config.n_embd, config.vocab_size)
+
+        ## init all weights
+        ## from karpathy
+        self.apply(self._init_weights)
+        for pn, p in self.named_parameters():
+          if pn.endswith('attention.weight'):
+            torch.nn.init.normal_(p, mean=0.0, std=0.02/math.sqrt(2 * config.n_layer))
+
+        # report number of parameters
+        print("number of parameters: %d" % (sum(p.nelement() for p in self.parameters()),))
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02 / math.sqrt(2 * config.n_layer))
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02 / math.sqrt(2 * config.n_layer))
+
+    def forward(self, idx, targets=None):
+        device = idx.device
+        B, T = idx.shape
+        tok_emb = self.token(idx)
+        position_ids = torch.arange(0, T, dtype = torch.long, device = device).unsqueeze(0)
+        pos_emb =  self.positional_embeddings(position_ids)
+        x = tok_emb + pos_emb
+        for block in self.blocks:
+          x = self.blocks(x)
+        x = self.ln_f(x)
+        logits = self.lnum_heads(x)
+
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B*T, C)
+            targets = targets.view(B*T)
+            loss = F.cross_entropy(logits, targets)
+
+        return logits, loss
+
+        
+    ## from karpathy's youtube videos.
+    def generate(self, idx, max_new_tokens):
+        # idx is (B, T) array of indices in the current context
+        for _ in range(max_new_tokens):
+            # crop idx to the last block_size tokens
+            idx_cond = idx[:, -config.block_size:]
+            # get the predictions
+            logits, loss = self(idx_cond)
+            # focus only on the last time step
+            logits = logits[:, -1, :] # becomes (B, C)
+            # apply softmax to get probabilities
+            probs = F.softmax(logits, dim=-1) # (B, C)
+            # sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
+            # append sampled index to the running sequence
+            idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
+        return idx
+
+
+config = LLaMAConfig(
+    block_size = 4,
+    vocab_size = len(chars),
+    n_head = 4,
+    n_layer = 4,
+    n_embd = 16)
+
+model = BabyGPTmodel(config)
+
+m = model.to(device)
+
+# create a PyTorch optimizer
+optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+
+for iter in range(max_iters):
+
+    # every once in a while evaluate the loss on train and val sets
+    if iter % eval_interval == 0 or iter == max_iters - 1:
+        losses = estimate_loss()
+        print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+
+    # sample a batch of data
+    xb, yb = get_batch('train')
+
+    # evaluate the loss
+    logits, loss = model(xb, yb)
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
+
+# generate from the model
+context = torch.zeros((1, 1), dtype=torch.long, device=device)
+print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
