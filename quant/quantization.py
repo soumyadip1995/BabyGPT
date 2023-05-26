2e2af5e2020c39bacea8b337a3897c3ae651dc4f
Soumyadip Nandi <30930746+soumyadip1995@users.noreply.github.com>
2023-05-26 20:35:12 +0530

    Updated quantization.py

--- quant/quantization.py
+++ quant/quantization.py
@@ -1,67 +1,60 @@
+## this is the code from ist daslab. License belongs to them.
+### Modify this to support your code
+''' The Quantizer class and Quant3Linear module are used for quantizing neural network weights and activations to reduce
+memory usage and improve inference speed. The Quantizer class finds the optimal scaling and zero-point values for
+quantization, while the Quant3Linear module performs quantized matrix multiplication using 3-bit integers. '''
+
+import numpy as np
 import torch
-import torch.nn as nn 
-import math
-import tqdm
-import numpy as np  
+import torch.nn as nn
 
 
-class GPTQQuantizer:
-    # The algorithm and code has been taken from  https://github.com/IST-DASLab/gptq/
-    # E. Frantar et al GPTQ: Accurate Post-training Compression for GPT, arXiv:2210.17323
-    # portions copyright by the authors licensed under the Apache License 2.0
+def quantize(x, scale, zero, maxq):
+    if maxq < 0:
+        return (x > scale / 2).float() * scale + (x < zero / 2).float() * zero
+    q = torch.clamp(torch.round(x / scale) + zero, 0, maxq)
+    return scale * (q - zero)
 
+class Quantizer(nn.Module):
 
-    def __init__(
+    def __init__(self, shape=1):
+        super(Quantizer, self).__init__()
+        self.register_buffer('maxq', torch.tensor(0))
+        self.register_buffer('scale', torch.zeros(shape))
+        self.register_buffer('zero', torch.zeros(shape))
+
+    def configure(
         self,
-        linear_module,
-        *,
-        bits,
-        perchannel=True,
-        sym=False,
-        blocksize=128,
-        percdamp=0.01,
-        groupsize=-1,
-        actorder=False
+        bits, perchannel=False, sym=True, 
+        mse=False, norm=2.4, grid=100, maxshrink=.8,
+        trits=False
     ):
-        assert isinstance(linear_module, torch.nn.Linear)
-
-        self.linear_module = linear_module
-        self.dev = self.linear_module.weight.device
-        self.rows = linear_module.weight.shape[0]
-        self.columns = linear_module.weight.shape[1]
-        self.H = torch.zeros((self.columns, self.columns), device=self.dev)
-        self.nsamples = 0
-        self.bits = bits
-        self.maxq = 2**bits - 1
+        self.maxq = torch.tensor(2 ** bits - 1)
         self.perchannel = perchannel
         self.sym = sym
-        self.blocksize = blocksize
-        self.percdamp = percdamp
-        self.groupsize = groupsize
-        self.actorder = actorder
-        self.tile_cols = self.columns if groupsize == -1 else groupsize
-        self.scales = torch.zeros(
-            (self.rows, (self.columns + self.tile_cols - 1) // self.tile_cols),
-            dtype=self.linear_module.weight.dtype,
-            device=self.dev,
-        )
-        self.zeros = torch.zeros_like(self.scales)
-        assert not (
-            self.actorder and self.groupsize != -1
-        ), "The permutation trick does not work for grouped quantization"
+        self.mse = mse
+        self.norm = norm
+        self.grid = grid
+        self.maxshrink = maxshrink 
+        if trits:
+            self.maxq = torch.tensor(-1) 
 
-    @staticmethod
-    def quantize_weight(x, scale, zero, maxq):
-        q = torch.clamp(torch.round(x / scale) + zero, 0, maxq)
-        x_rec = scale * (q - zero)
-        return x_rec
-
-    def find_params_weight(self, x):
+    def find_params(self, x, weight=False):
         dev = x.device
+        self.maxq = self.maxq.to(dev)
 
         shape = x.shape
         if self.perchannel:
-            x = x.flatten(1)
+            if weight:
+                x = x.flatten(1)
+            else:
+                if len(shape) == 4:
+                    x = x.permute([1, 0, 2, 3])
+                    x = x.flatten(1)
+                if len(shape) == 3:
+                    x = x.reshape((-1, shape[-1])).t()
+                if len(shape) == 2:
+                    x = x.t()
         else:
             x = x.flatten().unsqueeze(0)
 
@@ -78,121 +71,148 @@ class GPTQQuantizer:
         xmin[tmp] = -1
         xmax[tmp] = +1
 
-        scale = (xmax - xmin) / self.maxq
-        if self.sym:
-            zero = torch.full_like(scale, (self.maxq + 1) / 2)
+        if self.maxq < 0:
+          self.scale = xmax
+          self.zero = xmin
         else:
-            zero = torch.round(-xmin / scale)
+          self.scale = (xmax - xmin) / self.maxq
+          if self.sym:
+              self.zero = torch.full_like(self.scale, (self.maxq + 1) / 2)
+          else:
+              self.zero = torch.round(-xmin / self.scale)
 
+        if self.mse:
+            best = torch.full([x.shape[0]], float('inf'), device=dev)
+            for i in range(int(self.maxshrink * self.grid)):
+                p = 1 - i / self.grid 
+                xmin1 = p * xmin
+                xmax1 = p * xmax
+                scale1 = (xmax1 - xmin1) / self.maxq
+                zero1 = torch.round(-xmin1 / scale1) if not self.sym else self.zero
+                q = quantize(x, scale1.unsqueeze(1), zero1.unsqueeze(1), self.maxq)
+                q -= x
+                q.abs_()
+                q.pow_(self.norm)
+                err = torch.sum(q, 1)
+                tmp = err < best
+                if torch.any(tmp):
+                    best[tmp] = err[tmp]
+                    self.scale[tmp] = scale1[tmp]
+                    self.zero[tmp] = zero1[tmp]
         if not self.perchannel:
-            tmp = shape[0]
-            scale = scale.repeat(tmp)
-            zero = zero.repeat(tmp)
+            if weight:
+                tmp = shape[0]
+            else:
+                tmp = shape[1] if len(shape) != 3 else shape[2]
+            self.scale = self.scale.repeat(tmp)
+            self.zero = self.zero.repeat(tmp)
 
-        shape = [-1] + [1] * (len(shape) - 1)
-        scale = scale.reshape(shape)
-        zero = zero.reshape(shape)
-        return scale, zero
+        if weight:
+            shape = [-1] + [1] * (len(shape) - 1)
+            self.scale = self.scale.reshape(shape)
+            self.zero = self.zero.reshape(shape)
+            return
+        if len(shape) == 4:
+            self.scale = self.scale.reshape((1, -1, 1, 1))
+            self.zero = self.zero.reshape((1, -1, 1, 1))
+        if len(shape) == 3:
+            self.scale = self.scale.reshape((1, 1, -1))
+            self.zero = self.zero.reshape((1, 1, -1)) 
+        if len(shape) == 2:
+            self.scale = self.scale.unsqueeze(0)
+            self.zero = self.zero.unsqueeze(0)
 
-    def collect_input_stats(self, _1, inp, _2):
-        inp = inp[0].detach()
-        self.last_inp = inp
-        if len(inp.shape) == 2:
-            inp = inp.unsqueeze(0)
-        tmp = inp.shape[0]
-        if len(inp.shape) == 3:
-            inp = inp.reshape((-1, inp.shape[-1]))
-        inp = inp.t()
-        self.H *= self.nsamples / (self.nsamples + tmp)
-        self.nsamples += tmp
-        # inp = inp.float()
-        inp = math.sqrt(2 / self.nsamples) * inp.float()
-        # self.H += 2 / self.nsamples * inp.matmul(inp.t())
-        self.H += inp.matmul(inp.t())
+    def quantize(self, x):
+        if self.ready():
+            return quantize(x, self.scale, self.zero, self.maxq)
+        return x
 
-    def quantize(self):
-        W = self.linear_module.weight.detach().to(dtype=torch.float, copy=True)
+    def enabled(self):
+        return self.maxq > 0
 
-        scale, zero = self.find_params_weight(W)
-        self.scales[:] = scale
-        self.zeros[:] = zero
+    def ready(self):
+        return torch.all(self.scale != 0)
 
-        H = self.H
-        del self.H
-        dead = torch.diag(H) == 0
-        H[dead, dead] = 1
-        W[:, dead] = 0
-        if self.actorder:
-            perm = torch.argsort(torch.diag(H), descending=True)
-            W = W[:, perm]
-            H = H[perm][:, perm]
 
-        Losses = torch.zeros_like(W)
-        Q = torch.zeros_like(W)
+try:
+    import quant_cuda
+except:
+    print('CUDA extension not installed.')
 
-        damp = self.percdamp * torch.mean(torch.diag(H))
-        diag = torch.arange(self.columns, device=self.dev)
-        H[diag, diag] += damp
-        H = torch.linalg.cholesky(H)
-        H = torch.cholesky_inverse(H)
-        H = torch.linalg.cholesky(H, upper=True)
-        Hinv = H
+# Assumes layer is perfectly divisible into 1024 * 1024 blocks
+class Quant3Linear(nn.Module): 
 
-        for i1 in range(0, self.columns, self.blocksize):
-            i2 = min(i1 + self.blocksize, self.columns)
-            count = i2 - i1
-
-            W1 = W[:, i1:i2].clone()
-            Q1 = torch.zeros_like(W1)
-            Err1 = torch.zeros_like(W1)
-            Losses1 = torch.zeros_like(W1)
-            Hinv1 = Hinv[i1:i2, i1:i2]
-
-            for i in range(count):
-                w = W1[:, i]
-                d = Hinv1[i, i]
-
-                if self.groupsize != -1:
-                    if (i1 + i) % self.groupsize == 0:
-                        scale, zero = self.find_params_weight(
-                            W[:, (i1 + i) : (i1 + i + self.groupsize)]
-                        )
-                        self.scales[:, (i1 + i) // self.groupsize] = scale
-                        self.zeros[:, (i1 + i) // self.groupsize] = zeros
-
-                q = self.quantize_weight(w.unsqueeze(1), scale, zero, self.maxq)
-                q = q.squeeze(1)
-                assert q.dim() == 1
-                Q1[:, i] = q
-                Losses1[:, i] = (w - q) ** 2 / d**2
-
-                err1 = (w - q) / d
-                W1[:, i:] -= err1.unsqueeze(1).matmul(Hinv1[i, i:].unsqueeze(0))
-                Err1[:, i] = err1
-
-            Q[:, i1:i2] = Q1
-            Losses[:, i1:i2] = Losses1 / 2
-
-            W[:, i2:] -= Err1.matmul(Hinv[i1:i2, i2:])
-
-        if self.actorder:
-            invperm = torch.argsort(perm)
-            Q = Q[:, invperm]
-
-        weight = Q.reshape(self.linear_module.weight.shape).to(
-            self.linear_module.weight.data.dtype
+    def __init__(self, infeatures, outfeatures, faster=False):
+        super().__init__()
+        self.register_buffer('zeros', torch.zeros((outfeatures, 1)))
+        self.register_buffer('scales', torch.zeros((outfeatures, 1)))
+        self.register_buffer('bias', torch.zeros(outfeatures))
+        self.register_buffer(
+            'qweight', torch.zeros((infeatures // 32 * 3, outfeatures), dtype=torch.int)
         )
-        error = torch.sum(Losses).item()
+        self.faster = faster
 
-        q_module = ColBlockQuantizedLinear(
-            self.linear_module.in_features,
-            self.linear_module.out_features,
-            self.linear_module.bias is not None,
-            bits=self.bits,
-            tile_cols=self.groupsize,
-        ).to(self.dev)
-        q_module.scales = self.scales
-        q_module.zeros = self.zeros
-        q_module.pack_weight(weight)
-        q_module.bias = self.linear_module.bias
-        return q_module, error
+    def pack(self, linear, scales, zeros):
+        self.zeros = zeros * scales
+        self.scales = scales.clone()
+        self.bias = linear.bias.clone()
+
+        intweight = torch.round((linear.weight.data + self.zeros) / self.scales).to(torch.int)
+        intweight = intweight.t().contiguous()
+        intweight = intweight.numpy().astype(np.uint32)
+        qweight = np.zeros(
+            (intweight.shape[0] // 32 * 3, intweight.shape[1]), dtype=np.uint32
+        )
+        i = 0
+        row = 0
+        while row < qweight.shape[0]:
+            for j in range(i, i + 10):
+                qweight[row] |= intweight[j] << (3 * (j - i))
+            i += 10
+            qweight[row] |= intweight[i] << 30
+            row += 1
+            qweight[row] |= (intweight[i] >> 2) & 1
+            i += 1
+            for j in range(i, i + 10):
+                qweight[row] |= intweight[j] << (3 * (j - i) + 1)
+            i += 10
+            qweight[row] |= intweight[i] << 31
+            row += 1
+            qweight[row] |= (intweight[i] >> 1) & 0x3
+            i += 1
+            for j in range(i, i + 10):
+                qweight[row] |= intweight[j] << (3 * (j - i) + 2)
+            i += 10
+            row += 1
+
+        qweight = qweight.astype(np.int32)
+        self.qweight = torch.from_numpy(qweight) 
+
+    def forward(self, x):
+        if x.shape[-1] == x.numel():
+            outshape = list(x.shape)
+            y = self.bias.clone()
+            outshape[-1] = self.bias.numel()
+            dtype = x.dtype
+            if self.faster:
+                x = x.half()
+                quant_cuda.vecquant3matmul_faster(x, self.qweight, y, self.scales, self.zeros)
+            else:
+                x = x.float()
+                quant_cuda.vecquant3matmul(x, self.qweight, y, self.scales, self.zeros)
+            y = y.to(dtype)
+            return y.reshape(outshape)
+        raise ValueError('Only supports a single token currently.')
+
+def make_quant3(module, names, name='', faster=False):
+    if isinstance(module, Quant3Linear):
+        return
+    for attr in dir(module):
+        tmp = getattr(module, attr)
+        name1 = name + '.' + attr if name != '' else attr
+        if name1 in names:
+            setattr(
+                module, attr, Quant3Linear(tmp.in_features, tmp.out_features, faster=faster)
+            )
+    for name1, child in module.named_children():
+        make_quant3(child, names, name + '.' + name1 if name != '' else name1, faster=faster)
